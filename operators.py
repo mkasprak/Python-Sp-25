9ed42b607d747e89025dac51fd4b18958901be3c
Meri Kasprak <mkasprak@mchenry.edu>
2025-01-25 12:44:23 -0600

    operators

---
+++ operators.py
@@ -0,0 +1,17 @@
+"""
+    Using Python Operators
+"""
+
+# math
+print("addition", 5 + 5)
+print("subtraction", 10 - 3)
+print("multiplication", 10 * 3)
+print("division", 10 / 3)  # division
+print("floor division", 10 // 3)
+print("Modulus (remainder)", 10 % 4)  # modulus - gives the remainder
+print("exponent", 10**2)
+
+print(10 > 2)
+print(10 == 2)
+print(10 < 2)
+print(10 != 2)
