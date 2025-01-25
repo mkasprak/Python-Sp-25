9eab69121834ed03b7bf2f4043b425e971375d1d
Meri Kasprak <mkasprak@mchenry.edu>
2025-01-25 12:32:44 -0600

    operators

---
+++ data.py
@@ -0,0 +1,17 @@
+"""_summary_
+    explore python data types
+"""
+
+# Strings
+print("I'm a string")
+name = "String!"
+print(name)
+# Numeric literals
+print(42)
+age = 50
+print(age)
+
+# Booleans
+same = True
+print(same)
+print(age == 42)
