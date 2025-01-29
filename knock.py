91960733fa06eb52ff7f8fb74d1c3c6463c3a550
Meri Kasprak <mkasprak@mchenry.edu>
2025-01-29 12:16:04 -0600

    uploading joke file

---
+++ knock.py
@@ -0,0 +1,10 @@
+'''
+    Make Meri Laugh
+'''
+
+print("Why do chicken coops only have 2 doors?")
+print("Because if they had 4 doors")
+print("They would be sedans.")
+
+print("What did the fish say when it hit the wall")
+print("dam")
