d44a16cbe12e29929c231c9a50ffc46cb4cd1030
Meri Kasprak <mkasprak@mchenry.edu>
2025-01-29 13:40:08 -0600

    end of class

---
+++ symbols.py
@@ -0,0 +1,21 @@
+'''
+    operators
+'''
+
+print('4 + 4 =', 4 + 4)
+print("Meri " + "Kasprak")
+print('4 + 4 as strings', '4' + str(4))
+print('10 - 3 =', 10 - 3)
+print("2 * 6 = ", 2 * 6)
+print("10/3 = ", 10/3)
+print("10 // 3 =", 10//3)
+print("10 % 3 = ", 10 % 3)
+
+print(10 > 2)
+print(10 < 2)
+print(10 == 2)
+print(10 != 2)
+
+for x in range(10):
+    if x % 2 == 0:
+        print(x)
