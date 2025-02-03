7bb692fb8a1a9924e911d5337ff93a458299eec1
Meri Kasprak <mkasprak@mchenry.edu>
2025-02-03 13:28:37 -0600

    in class updates 2/3

--- symbols.py
+++ symbols.py
@@ -2,6 +2,12 @@
     operators
 '''
 
+"""_summary_
+    paste assignment requirements here for the docstring
+    any links to resources or AI help go here
+"""
+
+# equations
 print('4 + 4 =', 4 + 4)
 print("Meri " + "Kasprak")
 print('4 + 4 as strings', '4' + str(4))
@@ -9,7 +15,7 @@ print('10 - 3 =', 10 - 3)
 print("2 * 6 = ", 2 * 6)
 print("10/3 = ", 10/3)
 print("10 // 3 =", 10//3)
-print("10 % 3 = ", 10 % 3)
+print("10 % 3 = ", 10 % 3)  # modulus - remainder division
 
 print(10 > 2)
 print(10 < 2)
